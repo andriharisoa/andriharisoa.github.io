cb39364390db56019547f2484a44fed025823b75
NIAINA Andriharisoa <niaina-akata@goavana.com>
2024-04-27 06:56:56 +0300

    add code

---
+++ app.js
@@ -0,0 +1,8 @@
+function slider(anything) {
+  document.querySelector(".devops").src = anything
+}
+
+function couleur(color) {
+  const sec = document.querySelector(".global")
+  sec.style.background = color
+}
